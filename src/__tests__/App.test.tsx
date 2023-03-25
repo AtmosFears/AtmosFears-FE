5eb1bdd81f439188465b4fc4d246161573bfb231
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-03-25 22:51:45 +0100

    Add React setup

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,22 @@
+import { cleanup, render, screen } from '@testing-library/react';
+
+import App from '../App';
+
+// Example tests
+describe('App', () => {
+  afterEach(cleanup);
+
+  it('snapshot', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders a button with correct text', () => {
+    render(<App />);
+
+    const button = screen.queryByRole('button');
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('count is 0');
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tsconfigPaths({
+      projects: ['./tsconfig.json']
+    }),
+    eslint({
+      fix: true
+    }),
+    react()
+  ],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets/',
+      '@components': '/src/components/',
+      '@core': '/src/core/',
+      '@modules': '/src/modules/',
+      '@store': '/src/store/'
+    }
+  }
+});
