a4c625663105198276c0bc074a4d1bacae345b7e
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-05-09 22:34:15 +0200

    Add missing changes from master

--- src/components/shared/Map/index.tsx
+++ src/components/shared/Map/index.tsx
@@ -5,13 +5,31 @@ import MapPointer from './MapPointer';
 
 export default function Map() {
   return (
-    <MapContainer center={[50.05, 19.93]} zoom={12.5} className='h-full w-full'>
+    <MapContainer center={[50.05, 19.93]} zoom={12.5} className='w-full h-full'>
       <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
       <MapPointer
         center={[50.05, 19.93]}
-        radius={200}
+        radius={20}
         text='21'
         color='#c90e0e'
+        sensorData={{
+          date: new Date(),
+          name: 'Kraków, ul. Wrocławska 20',
+          data: [
+            {
+              name: 'PM1',
+              value: 21
+            },
+            {
+              name: 'PM2.5',
+              value: 21
+            },
+            {
+              name: 'PM10',
+              value: 0.003
+            }
+          ]
+        }}
       />
     </MapContainer>
   );
