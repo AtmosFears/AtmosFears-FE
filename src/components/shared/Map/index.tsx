634752427ebb3120e8c2a1e385b61db75cdbd7e9
AndrzejStarzyk <92925198+AndrzejStarzyk@users.noreply.github.com>
2023-05-24 15:30:18 +0200

    Update src/components/shared/Map/index.tsx
    Co-authored-by: Mateusz Łopaciński <52978053+MatiPl01@users.noreply.github.com>

--- src/components/shared/Map/index.tsx
+++ src/components/shared/Map/index.tsx
@@ -35,20 +35,16 @@ export default function Map() {
   return (
     <MapContainer center={[50.05, 19.93]} zoom={12.5} className='w-full h-full'>
       <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-      {sensorLocationData.length > 0
-        ? sensorLocationData.map(value =>
-            value.location.n && value.location.e ? (
-              <MapPointer
-                key={value.key}
-                center={[value.location.n, value.location.e]}
-                radius={20}
-                text='21'
-                color='#c90e0e'
-                sensorData={value.data}
-              />
-            ) : null
-          )
-        : null}
+      {sensorLocationData.map(value => (
+        <MapPointer
+          key={value.key}
+          center={[value.location.n, value.location.e]}
+          radius={20}
+          text='21'
+          color='#c90e0e'
+          sensorData={value.data}
+        />
+      ))}
     </MapContainer>
   );
 }
