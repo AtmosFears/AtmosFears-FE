dd4a82f25834b9e02a65709ca2609a5b01ae2d28
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-06-21 10:26:49 +0200

    FIx windrose chart not rerendering issue (#39)
    Add a workaround to force windrose chart rerender

--- src/components/WindRose/WindRoseChart.tsx
+++ src/components/WindRose/WindRoseChart.tsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from 'react';
 import { Chart, type ChartData } from 'react-windrose-chart';
 
 import { WINDROSE } from '@/constants';
+
+const WINDROSE_CHART_SIZE = 600;
 
 interface WindRoseChartProps {
   chartData: ChartData[];
 }
 
 function WindRoseChart({ chartData }: WindRoseChartProps) {
+  const [responsive, setResponsive] = useState(true);
+
+  useEffect(() => {
+    setResponsive(r => !r);
+    setTimeout(() => {
+      setResponsive(r => !r);
+    }, 100);
+  }, [chartData]);
+
   return (
     <div className='m-4 flex'>
       {chartData && (
@@ -14,9 +26,9 @@ function WindRoseChart({ chartData }: WindRoseChartProps) {
           <Chart
             chartData={chartData}
             columns={WINDROSE.columns}
-            width={600}
-            height={600}
-            responsive
+            width={WINDROSE_CHART_SIZE - Math.random()}
+            height={WINDROSE_CHART_SIZE - Math.random()}
+            responsive={responsive}
           />
         </div>
       )}
